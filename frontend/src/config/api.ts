bd56794e1e02e97195d74e393d02cd15ef962f4a
VijayVPatil13 <vijayvpatil13@gmail.com>
2026-05-18 12:53:34 +0530

    api fix in depolyment

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,3 +1,4 @@
-export const API_BASE = import.meta.env.VITE_API_BASE_URL || (import.meta.env.DEV ? 'http://localhost:8000' : '/');
-
+export const API_BASE =
+    import.meta.env.VITE_API_BASE_URL ||
+    "https://stock-market-backend-app.onrender.com";
 export const API_DOCS_URL = `${API_BASE}/docs`;
